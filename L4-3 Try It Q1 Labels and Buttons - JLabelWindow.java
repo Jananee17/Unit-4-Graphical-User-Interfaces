c0a7404059d964269ec7a8f113859b5f07909126
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-06 15:25:21 -0500

    L4-3 Try It Q1 Labels and Buttons JLabelWindow
    This is my answer to question 1 try it lesson 4-3 labels and buttons J Label Window in unit 4.

--- L4-3 Try It Q1 Labels and Buttons - JLabelWindow.java
+++ L4-3 Try It Q1 Labels and Buttons - JLabelWindow.java
@@ -11,3 +11,13 @@ public class JLabelWindow
        appWindow.setVisible( true );
    }
 }
+
+/* Write a GUI application to help track student success.  
+* Your solution should look something like this.  
+* Include two buttons to keep track of the number of passes and fails, 
+* with a label to give an ongoing status update.  
+* Use a regular (not anonymous) inner class to take care of the 
+* event handling for both buttons.  Hint: the JLabel class has a 
+* method that you can use to update the text on a JLabel object 
+* any time after it has been instantiated.
+*/
